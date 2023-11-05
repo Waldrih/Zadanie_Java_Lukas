25e459b8ef3192a425d68ecb1891b89a941600e8
Waldrih <waldrih_wow3@interia.pl>
2023-11-05 15:42:40 +0100

    Added Client constructor, getter and setter.

--- LukasSklep/src/Client.java
+++ LukasSklep/src/Client.java
@@ -2,4 +2,34 @@ public class Client {
     String name;
     String address;
     String email;
+
+    public Client(String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
